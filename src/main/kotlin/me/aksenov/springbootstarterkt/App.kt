4a761a66e7bf727399fe0625a8ad3c5c0d2c1126
v-aksenov <50148578+v-aksenov@users.noreply.github.com>
2021-06-05 19:08:59 +0300

    Initial commit

---
+++ src/main/kotlin/me/aksenov/springbootstarterkt/App.kt
@@ -0,0 +1,11 @@
+package me.aksenov.springbootstarterkt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootStarterKtApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringBootStarterKtApplication>(*args)
+}
---
+++ src/test/kotlin/me/aksenov/springbootstarterkt/AppTest.kt
@@ -0,0 +1,13 @@
+package me.aksenov.springbootstarterkt
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AppTest {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
